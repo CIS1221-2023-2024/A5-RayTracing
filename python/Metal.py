d43c45faee59b4352ac5ef79091498dbeffa9eca
Ale866 <87303299+Ale866@users.noreply.github.com>
2023-12-23 16:26:59 +0100

    implemented fuzziness on metal material

--- python/Metal.py
+++ python/Metal.py
@@ -4,12 +4,12 @@ from Vec3 import Color, Vec3
 
 
 class Metal(IMaterial):
-    def __init__(self, albedo : Color):
+    def __init__(self, albedo : Color, fuzz : float):
+        self.fuzz = fuzz if fuzz < 1 else 1
         self.albedo = albedo
 
     def scatter(self, r_in, rec, attenuation, scattered):
         reflected = Vec3.reflect(Vec3.unit_vector(r_in.direction), rec.normal)
-        r = Ray(rec.p,reflected)
-        scattered.copy(r)
+        scattered.copy(Ray(rec.p, reflected + self.fuzz * Vec3.random_unit_vector()))
         attenuation.copy(self.albedo)
-        return True
+        return scattered.direction.dot(rec.normal) > 0
--- python/main.py
+++ python/main.py
@@ -5,19 +5,25 @@ from Camera import Camera
 from Lambertian import Lambertian
 from Metal import Metal
 
-# World
 def main():
     world = HittableList()
-    world.add(Sphere(Point3(0,0,-1), 0.5, Metal(Color(0.1,0.2,0.5))))
-    world.add(Sphere(Point3(0,-100.5,-1), 100, Metal(Color(0.8,0.8,0.0))))
 
+    material_ground = Lambertian(Color(0.8, 0.8, 0.0))
+    material_center = Lambertian(Color(0.7, 0.3, 0.3))
+    material_left = Metal(Color(0.8, 0.8, 0.8), 0.3)
+    material_right = Metal(Color(0.8, 0.6, 0.2), 1.0)
+
+    world.add(Sphere(Point3(0.0, -100.5, -1.0), 100.0, material_ground))
+    world.add(Sphere(Point3(0.0, 0.0, -1.0), 0.5, material_center))
+    world.add(Sphere(Point3(-1.0, 0.0, -1.0), 0.5, material_left))
+    world.add(Sphere(Point3(1.0, 0.0, -1.0), 0.5, material_right))
+    
     cam = Camera()
-
     cam.aspect_ratio = 16.0 / 9.0
     cam.image_width = 400
     cam.samples_per_pixel = 10
 
     cam.render(world)
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     main()
