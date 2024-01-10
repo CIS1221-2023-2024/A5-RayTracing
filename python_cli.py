f3c7828f7b62da75944474e2cb95571d8ac9ec90
giuszeppe <dimarcogiuseppe04@gmail.com>
2024-01-10 15:01:25 +0100

    improved cli help message for sphere_numbers

--- python_cli.py
+++ python_cli.py
@@ -12,7 +12,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument("-s", "--samples", type=int, default=10, help="Number of samples per pixel")
 parser.add_argument("-w", "--width", type=int, default=100, help="Width of the output image")
 parser.add_argument("-d", "--depth", type=int, default=10, help="Depth of the shadow recursion")
-parser.add_argument("-n", "--number", type=int, default=4, help="Number of spheres")
+parser.add_argument("-n", "--number", type=int, default=4, help="Number of spheres (total_spheres =  (2 * N )**2)")
 parser.add_argument("-o", "--output", type=str, default="output.ppm", help="Output file name")
 parser.add_argument("-np", "--numpy", type=bool, default=False, help="Use numpy for calculations")
 parser.add_argument("-r", '--ratio', type=float, default=16.0 / 9.0, help="Aspect ratio of the image")
