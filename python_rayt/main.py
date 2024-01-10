e8619b46abaa900e028bc90e943ef1eb9ec872e7
giuszeppe <dimarcogiuseppe04@gmail.com>
2024-01-10 14:25:12 +0100

    simplified main code using rendered

--- python_rayt/main.py
+++ python_rayt/main.py
@@ -1,31 +1,23 @@
-from geometries.Vec3 import Color, Point3
-from rendering.Hittable import HittableList
-from rendering.Camera import Camera
-from geometries.Sphere import Sphere
-from materials.Lambertian import Lambertian
-from materials.Metal import Metal
-from materials.Dielectric import Dielectric
+from python_rayt.geometries.Vec3 import Color, Point3
+from python_rayt.rendering.Hittable import HittableList
+from python_rayt.utilities import random_double, random_double_range
+from python_rayt.rendering.Camera import Camera
+from python_rayt.geometries.Sphere import Sphere
+from python_rayt.materials.Lambertian import Lambertian
+from python_rayt.materials.Metal import Metal
+from python_rayt.materials.Dielectric import Dielectric
+from python_rayt.Renderer import Renderer
 
 def main():
-    world = HittableList()
+    samples = 100
+    width = 200
+    depth = 50
+    number = 11
+    output = "output.ppm"
+    numpy = False
+    ratio = 16/9
 
-    material_ground = Lambertian(Color(0.8, 0.8, 0.0))
-    material_center = Lambertian(Color(0.1, 0.2, 0.5))
-    material_left = Dielectric(1.5)
-    material_right = Metal(Color(0.8, 0.6, 0.2), 0.0)
+    Renderer.render(samples, width, depth, number, output, numpy, ratio)
 
-    world.add(Sphere(Point3(0.0, -100.5, -1.0), 100.0, material_ground))
-    world.add(Sphere(Point3(0.0, 0.0, -1.0), 0.5, material_center))
-    world.add(Sphere(Point3(-1.0, 0.0, -1.0), 0.5, material_left))
-    world.add(Sphere(Point3(-1.0, 0.0, -1.0), -0.4, material_left))
-    world.add(Sphere(Point3(1.0, 0.0, -1.0), 0.5, material_right))
-    
-    cam = Camera()
-    cam.aspect_ratio = 16.0 / 9.0
-    cam.image_width = 400
-    cam.samples_per_pixel = 10
-
-    cam.render(world)
-
-if __name__ == '__main__':
+if __name__=="__main__":
     main()
