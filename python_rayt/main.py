e461f0fda981be3291a43e56727f7e294d1b147b
Ale866P <alessandromancini866@gmail.com>
2024-01-11 12:34:52 +0100

    removed unused main.py imports

--- python_rayt/main.py
+++ python_rayt/main.py
@@ -1,11 +1,3 @@
-from python_rayt.geometries.Vec3 import Color, Point3
-from python_rayt.rendering.Hittable import HittableList
-from python_rayt.utilities import random_double, random_double_range
-from python_rayt.rendering.Camera import Camera
-from python_rayt.geometries.Sphere import Sphere
-from python_rayt.materials.Lambertian import Lambertian
-from python_rayt.materials.Metal import Metal
-from python_rayt.materials.Dielectric import Dielectric
 from python_rayt.Renderer import Renderer
 
 def main():
