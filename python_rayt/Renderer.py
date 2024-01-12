0e7ace53f2e5d7fce397dbdfaa1f8e2de5ec7423
giuszeppe <dimarcogiuseppe04@gmail.com>
2024-01-12 14:59:59 +0100

    created better scene for becnhmarking

--- python_rayt/Renderer.py
+++ python_rayt/Renderer.py
@@ -22,6 +22,7 @@ class Renderer:
             for b in range(-number,number):
                 choose_mat = random_double()
                 center = Point3(a + 0.9*random_double(), 0.2, b + 0.9*random_double())
+                radius = random_double_range(0.1, 0.3)
 
                 if ((center - Point3(4, 0.2, 0)).length() > 0.9):
                     sphere_material = None
@@ -29,18 +30,19 @@ class Renderer:
                     if (choose_mat < 0.8):
                         albedo = Color.random(0,1) * Color.random(0,1)
                         sphere_material = Lambertian(albedo)
-                        world.add(Sphere(center, 0.2, sphere_material))
+                        world.add(Sphere(center, radius, sphere_material))
                     elif (choose_mat < 0.95):
                         albedo = Color.random(0.5, 1)
                         fuzz = random_double_range(0, 0.5)
                         sphere_material = Metal(albedo, fuzz)
-                        world.add(Sphere(center, 0.2, sphere_material))
+                        world.add(Sphere(center, radius, sphere_material))
                     else:
                         sphere_material = Dielectric(1.5)
-                        world.add(Sphere(center, 0.2, sphere_material))
+                        world.add(Sphere(center, radius, sphere_material))
                     
 
-
+        material3 = Metal(Color(0.7, 0.6, 0.5), 0.0)
+        world.add(Sphere(Point3(4, 1, 0), 1.0, material3))
 
         cam = Camera()
         cam.aspect_ratio = ratio
@@ -48,12 +50,9 @@ class Renderer:
         cam.samples_per_pixel = samples
         cam.max_depth = depth
         cam.filename = output
-        cam.vfov     = 90
-        cam.lookfrom = Point3(-2,2,1)
-        cam.lookat   = Point3(0,0,-1)
+        cam.vfov     = 20
+        cam.lookfrom = Point3(13,2,3)
+        cam.lookat   = Point3(0,0,0)
         cam.vup      = Vec3(0,1,0)
-
-        print(world)
-        
 
         cam.render(world)
