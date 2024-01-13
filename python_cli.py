5737f453bcfe40ec0ea561242e8be4f5829bdd9f
Ale866P <alessandromancini866@gmail.com>
2024-01-13 23:14:19 +0100

    added main to the python_cli, rendered two images as an example

--- python_cli.py
+++ python_cli.py
@@ -1,23 +1,28 @@
 import argparse
+from multiprocessing import freeze_support
 
 from python_rayt.Renderer import Renderer as PythonRenderer
 #from numpy_rayt.Renderer import Renderer as NumpyRenderer
  
-parser = argparse.ArgumentParser(
-    prog="RaytracingCLI",
-    description="A simple CLI for rendering images using our raytracer.",
-    epilog="This is the epilog. It is a multiline string"
-)
+def main():
+    parser = argparse.ArgumentParser(
+        prog="RaytracingCLI",
+        description="A simple CLI for rendering images using our raytracer.",
+        epilog="This is the epilog. It is a multiline string"
+    )
  
-parser.add_argument("-s", "--samples", type=int, default=10, help="Number of samples per pixel")
-parser.add_argument("-w", "--width", type=int, default=100, help="Width of the output image")
-parser.add_argument("-d", "--depth", type=int, default=10, help="Depth of the shadow recursion")
-parser.add_argument("-n", "--number", type=int, default=4, help="Number of spheres (total_spheres =  (2 * N )**2)")
-parser.add_argument("-o", "--output", type=str, default="output.ppm", help="Output file name")
-parser.add_argument("-np", "--numpy", type=bool, default=False, help="Use numpy for calculations")
-parser.add_argument("-r", '--ratio', type=float, default=16.0 / 9.0, help="Aspect ratio of the image")
-parser.add_argument("-wr","--workers", type=int, default=4, help="Number of workers")
+    parser.add_argument("-s", "--samples", type=int, default=10, help="Number of samples per pixel")
+    parser.add_argument("-w", "--width", type=int, default=100, help="Width of the output image")
+    parser.add_argument("-d", "--depth", type=int, default=10, help="Depth of the shadow recursion")
+    parser.add_argument("-n", "--number", type=int, default=4, help="Number of spheres (total_spheres =  (2 * N )**2)")
+    parser.add_argument("-o", "--output", type=str, default="output.ppm", help="Output file name")
+    parser.add_argument("-np", "--numpy", type=bool, default=False, help="Use numpy for calculations")
+    parser.add_argument("-r", '--ratio', type=float, default=16.0 / 9.0, help="Aspect ratio of the image")
+    parser.add_argument("-wr","--workers", type=int, default=4, help="Number of workers")
  
-args = parser.parse_args().__dict__
-renderer = PythonRenderer()
-renderer.render(**args)
+    args = parser.parse_args().__dict__
+    renderer = PythonRenderer()
+    renderer.render(**args)
+
+if __name__ == '__main__':
+    main()
